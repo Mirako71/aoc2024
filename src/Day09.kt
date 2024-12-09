ef175563ee4c95a93c92416296736f4fa424e43f
Stefan Schlechtweg <stefan.schlechtweg@hs-anhalt.de>
2024-12-09 20:29:50 +0100

    Day09 part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -30,12 +30,52 @@ fun main() {
             .sumOf { (index, block) -> if (block is FileBlock) index * block.id.toLong() else 0L }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
+    fun part2(input: List<String>): Long {
+        // create a different disk map, this time with sized blocks
+        val diskMap = buildList {
+            var id = 0
+            input.first().windowed(2, 2, true).forEach {
+                add(FileSegment(id, it[0].digitToInt()))
+                if (it.length > 1)add(EmptySegment(it[1].digitToInt()))
+                id++
+            }
+        }.toMutableList()
 
-    val testInput = listOf("2333133121414131402")
-    check(part1(testInput) == 1928L)
+        var left: Int
+        var right = diskMap.indexOfLast { it is FileSegment }
+        while(true) {
+            if ((diskMap[right] as FileSegment).id == 0) break
+
+            // den ersten leeren Platz mit der richtigen Größe suchen
+            val sizeNeeded = (diskMap[right] as FileSegment).size
+            left = diskMap.indexOfFirst { it is EmptySegment && it.size >= sizeNeeded }
+            if (left != -1 && left < right) {
+                // Inhalte tauschen und leeren Platz neu berechnen
+                val newFreeSpace = (diskMap[left] as EmptySegment).size - sizeNeeded
+                diskMap[left] = diskMap[right]
+                diskMap[right] = EmptySegment(sizeNeeded)
+                if (newFreeSpace > 0) diskMap.add(left + 1, EmptySegment(newFreeSpace))
+
+            }
+            right--
+            while (diskMap[right] !is FileSegment) right--
+        }
+
+        var index = 0
+        var checksum = 0L
+        for (segment in diskMap) {
+            when (segment) {
+                is EmptySegment -> index += segment.size
+                is FileSegment -> {
+                    (index ..< index + segment.size)
+                        .fold(0L) { acc, i -> acc + i * segment.id.toLong() }.also { checksum += it }
+                    index += segment.size
+                }
+            }
+        }
+
+        return checksum
+    }
 
     val input = readInput("Day09")
     println(part1(input))
@@ -45,3 +85,7 @@ fun main() {
 sealed interface DiskBlock
 class EmptyBlock : DiskBlock
 data class FileBlock(val id: Int): DiskBlock
+
+sealed interface DiskSegment
+data class FileSegment(val id: Int, val size: Int): DiskSegment
+data class EmptySegment(val size: Int): DiskSegment
